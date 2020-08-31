7de44111f672e85240f904bc74398553bf6d28f4
Fatima Nguyen <fatima3455@hotmail.com>
2020-08-31 19:57:20 +0000

    update redirect link

--- express_server.js
+++ express_server.js
@@ -39,7 +39,14 @@ app.get('/urls/new', (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
